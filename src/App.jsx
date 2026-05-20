fc9d985a14ccd804eaac4c576f61fff8ac1b7c25
Josh Vardy <jvardy11@live.com>
2026-05-19 20:52:42 -0400

    Remove Analytics temporarily to restore site

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/react';
 import Layout from './components/layout/Layout';
 
 // Eager load home
@@ -42,7 +41,6 @@ export default function App() {
           </Routes>
         </Suspense>
       </Layout>
-      <Analytics />
     </BrowserRouter>
   );
 }
